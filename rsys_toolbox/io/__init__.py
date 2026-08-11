02d0aed7cd68a0f4e5483e214f4e9c55c7610d72
Pierre Lasorak <pierre.lasorak@networkrail.co.uk>
2026-08-11 09:40:24 +0100

    include things in __init__.opy

--- rsys_toolbox/io/__init__.py
+++ rsys_toolbox/io/__init__.py
@@ -1,1 +1,12 @@
-"""IO helpers for reading and typing Eval Manager data exports."""
+"""Public IO API for reading and typing Eval Manager data exports."""
+
+from rsys_toolbox.io.data_types import EvalManagerData
+from rsys_toolbox.io.eval_manager import load
+from rsys_toolbox.io.eval_manager_format import SCHEMA_EVAL_MANAGER_PANDAS, SCHEMA_EVAL_MANAGER_POLARS
+
+__all__ = [
+	"EvalManagerData",
+	"SCHEMA_EVAL_MANAGER_PANDAS",
+	"SCHEMA_EVAL_MANAGER_POLARS",
+	"load",
+]
